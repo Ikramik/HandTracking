f008d766af3ca918aff7706f34ae383661c81adc
Ikram Bekkaoui <ikkram5222@gmail.com>
2026-03-31 16:03:32 +0200

    Finished clicking mouse

--- AiVirtualMouse.py
+++ AiVirtualMouse.py
@@ -6,18 +6,19 @@ import autopy
 
 
 #######################
-
 wCam,hCam=648,488
-
+frameR = 100 #Frame reduction
+smoothening = 10
 #########################
 
 
 
-
+pTime=0
+plocX, plocY = 0,0
+clocX, clocY = 0,0
 cap = cv2.VideoCapture(0)
 cap.set(3,wCam)
 cap.set(4,hCam)
-pTime=0
 detector = htm.handDetector(maxHands=1)
 wScr, hScr = autopy.screen.size()
 #print(wScr,hScr)
@@ -41,18 +42,29 @@ while True:
         fingers = detector.fingersUp()
         #print(fingers)
         #4- Only index finger : Moving Mode
+        cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR), (255, 0, 255), 2)
         if fingers[1]==1 and fingers[2]==0:
             #5- Convert Coordinates
-            x3 = np.interp(x1,(0,wCam),(0,wScr))
-            y3 = np.interp(y1,(0,wCam),(0,wScr))
-            #6- smoothe Values
-            #7- Move Mouse
-            autopy.mouse.move(wScr-x3,y3)
-            cv2.circle(img,(x1,y1), 15, (255, 0, 255), cv2.FILLED)
+            x3 = np.interp(x1,(frameR,wCam-frameR),(0,wScr-1))
+            y3 = np.interp(y1,(frameR,hCam-frameR),(0,hScr-1))
+            #6- smoothen Values
+            clocX = plocX +(x3 - plocX) /smoothening
+            clocY = plocY +(y3 - plocY) /smoothening
 
+            #7- Move Mouse
+            autopy.mouse.move(wScr-clocX,clocY)
+            cv2.circle(img,(x1,y1), 15, (255, 0, 255), cv2.FILLED)
+            plocX,plocY = clocX, clocY
         #8- Both Index and middle fingers are up : Clicking Mode
-        #9- Find distance between fingers
-        #10- Click mouse if distance is short
+        if fingers[1]==1 and fingers[2]==1:
+            # 9- Find distance between fingers
+            length, img, lineInfo = detector.findDistance(8,12, img)
+            # 10- Click mouse if distance is short
+            if length <40:
+                cv2.circle(img,(lineInfo[4],lineInfo[5]),
+                           15,(0,255,0), cv2.FILLED)
+                autopy.mouse.click()
+
 
 
 
